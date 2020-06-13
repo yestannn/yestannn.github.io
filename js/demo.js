00e12c7a7d1a25c68ecf410d6f4fbd82b119ca87
yestannn <55324492+yestannn@users.noreply.github.com>
2020-06-13 11:19:30 +0600

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+	$('#demo_1').t({
+		beep:false,
+		caret:'<span style="color: #00C853;">|</span>',
+		speed:70,
+		repeat: true,
+		typing:function(elm,chr){
+
+		}
+	});
+}, 100);
